73dd64c254afc9e95748ead55e5b45651c14d87d
Lorenzo Kappeler <me@lnk.codes>
2018-02-02 17:30:02 +0100

    refactor(index): Use async in index file

--- index.js
+++ index.js
@@ -4,51 +4,43 @@ module.exports = function(app, optionsToMerge) {
     const options = Object.assign({ mountPath: '/getUserPermissionTree' }, optionsToMerge);
     const permissionTree = new PermissionTree(app.models, app.remotes());
 
-    app.get(options.mountPath, (req, res) => {
-        let currentUser;
+    app.get(options.mountPath, async(req, res) => {
+        try {
+            const user = await new Promise((resolve, reject) => {
+                app.models.AccessToken.findForRequest(req, {}, (error, accesstoken) => {
+                    if (error) reject(error);
 
-        new Promise((resolve, reject) => {
-            app.models.AccessToken.findForRequest(req, {}, (error, accesstoken) => {
-                if (error) reject(error);
+                    if (accesstoken === undefined) {
+                        res.status(401);
+                        res.send({
+                            Error: 'Unauthorized',
+                            Message: 'You need to be authenticated to access this endpoint',
+                        });
 
-                if (accesstoken === undefined) {
-                    res.status(401);
-                    res.send({
-                        Error: 'Unauthorized',
-                        Message: 'You need to be authenticated to access this endpoint',
-                    });
+                        return;
+                    }
 
-                    return;
-                }
-
-                resolve(app.models.User.findById(accesstoken.userId));
-            });
-        })
-            .then((user) => {
-                currentUser = user;
-                return app.models.Role.getRoles({
-                    principalType: app.models.RoleMapping.USER,
-                    principalId: currentUser.id,
-                });
-            })
-            .then((roles) => {
-                currentUser.userGroups = roles;
-
-                return permissionTree.getPermissionsForUser(currentUser);
-            })
-            .then((userPermissions) => {
-                res.setHeader('Content-Type', 'application/json');
-                res.status(200)
-                    .send(userPermissions);
-            })
-            .catch((error) => {
-                console.log('error.message', error.message);
-
-                res.status(500);
-                res.send({
-                    Error: 'Internal Server error',
-                    Message: JSON.stringify(error),
+                    resolve(app.models.User.findById(accesstoken.userId));
                 });
             });
+            user.userGroups = await app.models.Role.getRoles({
+                principalType: app.models.RoleMapping.USER,
+                principalId: user.id,
+            });
+
+            const userPermissions = await permissionTree.getPermissionsForUser(user);
+
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200)
+                .send(userPermissions);
+
+        } catch (error) {
+            console.error(error);
+
+            res.status(500);
+            res.send({
+                Error: 'Internal Server error',
+            });
+        }
     });
 };
